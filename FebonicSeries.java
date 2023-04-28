b483469dbc5916754b4674b13c6c63b151c364c2
sheetal <sheetalsaini15@gmail.com>
2023-04-28 18:00:10 +0530

    initial commit

---
+++ FebonicSeries.java
@@ -0,0 +1,12 @@
+class FebonicSeries
+{
+	public static void main(String s[])
+	{
+		int feb = 3, result=0;
+		for(int i=0;i<=3; i++)
+		{
+			result =i+result;
+			System.out.println("series is ::"+result);
+		}
+	}
+}
